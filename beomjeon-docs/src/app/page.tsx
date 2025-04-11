5a2f41697b35bb37b7f24fb3bb88400b13c25dd2
bmpark <club20608@gmail.com>
2025-04-11 10:31:11 +0900

    파일구조 변경

--- beomjeon-docs/src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getAllDocs } from "@/lib/markdown";
-import ClientHome from "./ClientHome";
-
-export default function Page() {
-    const docs = getAllDocs(); // 서버에서 미리 가져와서
-    return <ClientHome initialDocs={docs} />; // 클라이언트 컴포넌트에 전달
-}
--- beomjeon-docs/src/app/search/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getAllDocsDetailed } from "@/lib/markdown";
-import SearchPage from "./SearchClient";
-
-export default async function Page() {
-    const docs = getAllDocsDetailed();
-    return <SearchPage docs={docs} />;
-}
