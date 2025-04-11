41f973f84b5401a9500ea880bf369b493fd5427d
bmpark <club20608@gmail.com>
2025-04-11 14:23:17 +0900

    타입 오류 수정

--- apps/docs/src/app/[...slug]/page.tsx
+++ apps/docs/src/app/[...slug]/page.tsx
@@ -2,7 +2,7 @@ import { getDocBySlug } from "@/lib/markdown";
 import { notFound } from "next/navigation";
 import CopyableCode from "@@/components/CopyableCode";
 
-// JSX.Element 타입으로 명시적 타입 선언을 하지 않습니다
+// Next.js App Router의 페이지 컴포넌트
 export default async function DocPage({ params }: any) {
     const slugPath = params.slug.join("/");
     try {
